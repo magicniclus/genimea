9518b2fe0a445c4bab08a75b988717cbd4b48e79
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-13 13:53:17 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        text: "#1E2849",
+        textBlue: "#6B95AE",
+        backgroundBlue: "#99D1E5",
+        yellow: "#FDBF65",
+        white: "#FFFFFF",
+        pink: "#F894C0",
+        red: "#EE4857",
+      },
     },
   },
   plugins: [],
