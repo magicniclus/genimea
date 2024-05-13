6df40435f43f0a04e029fff2691286441c23751d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-13 19:53:04 +0200

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+const Header = () => {
+  return (
+    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto text-center">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          <span className="font-bold">Genimea</span> c’est quoi ?
+        </h2>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Avec <span className="font-bold">Genimea</span>, découvrez le test de{" "}
+          <span className="font-bold">QI ultra-précis</span>, fruit de plus de 7
+          ans de recherche, et approuvé par{" "}
+          <span className="font-bold">l&apos;International HP Academy</span>.
+        </p>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Pourquoi <span className="font-bold">Genimea</span> ?
+        </p>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          <span className="font-bold">+</span>Test Ultra-Précis : Profitez
+          d&apos;un test de QI basé sur des années de recherche scientifique.
+          <br /> <span className="font-bold">+</span>Validation Internationale :
+          Approuvé par l&apos;International HP Academy, votre score reflétera
+          véritablement votre potentiel intellectuel.
+          <br />
+          <span className="font-bold">+</span>Rapport Complet : Recevez un
+          rapport détaillé de vos résultats, incluant des analyses approfondies.
+        </p>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Découvrez votre véritable potentiel intellectuel dès aujourd&apos;hui
+          avec
+          <span className="font-bold">Genimea</span>.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
