3fd511d8708d3cb47364ad5bd1f99187430184b9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-21 17:44:30 +0200

    Update Container.tsx

--- src/components/test/Container.tsx
+++ src/components/test/Container.tsx
@@ -5,7 +5,7 @@ type ContainerProps = {
 const Container = (props: ContainerProps) => {
   return (
     <section className="w-full px-4 md:px-6 mt-5">
-      <div className="max-w-5xl w-full mx-auto md:bg-slate-100 rounded-xl flex flex-col items-center px-12 py-12">
+      <div className="max-w-5xl w-full mx-auto md:bg-slate-100 rounded-xl flex flex-col items-center px-12 py-12 shadow-md">
         {props.children}
       </div>
     </section>
