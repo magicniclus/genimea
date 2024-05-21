37d65f2a92ee6cdb38ee645c49c2c079fce5a4d5
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-21 17:44:32 +0200

    Update ContainerTest.tsx

--- src/components/test/ContainerTest.tsx
+++ src/components/test/ContainerTest.tsx
@@ -1,5 +1,5 @@
 const ContainerTest = () => {
-  return <div className="w-full min-h-[300px] border rounded-md mt-5 "></div>;
+  return <div className="w-full min-h-[300px] border rounded-md mt-5"></div>;
 };
 
 export default ContainerTest;
