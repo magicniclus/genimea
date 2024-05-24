54e79fe0258ed66365e2ab7ccecb2368c39efd32
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-24 12:07:31 +0200

    Update page.tsx

--- src/app/resultats/page.tsx
+++ src/app/resultats/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import SubscriptionForm from "@/components/stripe/SubscriptionForm";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+);
+
 const page = () => {
   return (
     <div>
       <h1>Resultat</h1>
+      <Elements stripe={stripePromise}>
+        <SubscriptionForm />
+      </Elements>
     </div>
   );
 };
