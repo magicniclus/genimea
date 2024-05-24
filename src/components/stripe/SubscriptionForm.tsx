101c607672da0bda2172b8c53fc84cb349ab0ac8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-24 18:47:28 +0200

    Update SubscriptionForm.tsx

--- src/components/stripe/SubscriptionForm.tsx
+++ src/components/stripe/SubscriptionForm.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import {
+  CardCvcElement,
+  CardExpiryElement,
+  CardNumberElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { useState } from "react";
 
@@ -24,7 +30,7 @@ const SubscriptionForm: React.FC = () => {
 
     setLoading(true);
 
-    const cardElement = elements.getElement(CardElement);
+    const cardElement = elements.getElement(CardNumberElement);
 
     if (!cardElement) {
       setError("Card element not found");
@@ -70,19 +76,89 @@ const SubscriptionForm: React.FC = () => {
     setLoading(false);
   };
 
+  const elementOptions = {
+    style: {
+      base: {
+        fontSize: "16px",
+        color: "#32325d",
+        "::placeholder": {
+          color: "#a0aec0",
+        },
+        iconStyle: "solid",
+      },
+      invalid: {
+        color: "#fa755a",
+        iconColor: "#fa755a",
+      },
+    },
+    hideIcon: false,
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="space-y-4 w-full">
+      <label
+        htmlFor="cardNumber"
+        className="block text-sm font-medium text-gray-700"
+      >
+        User Email
+      </label>
       <input
         type="email"
         placeholder="Email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         required
+        className="w-full px-3 py-3 text-[#a0aec0] border rounded-md focus:outline-none focus:ring-none mb-5"
       />
-      <CardElement />
-      {error && <div>{error}</div>}
-      <button type="submit" disabled={!stripe || loading}>
-        {loading ? "Processing..." : "Subscribe"}
+      {/* <div className="w-[70%] h-0.5 bg-slate-200 mx-auto" /> */}
+      <div className="space-y-2">
+        <label
+          htmlFor="cardNumber"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Card Number
+        </label>
+        <CardNumberElement
+          id="cardNumber"
+          options={elementOptions}
+          className="w-full p-3 border rounded-md"
+        />
+      </div>
+      <div className="flex md:space-x-4 flex-col md:flex-row">
+        <div className="w-full md:w-1/2">
+          <label
+            htmlFor="cardExpiry"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Expiration Date (MM/YY)
+          </label>
+          <CardExpiryElement
+            id="cardExpiry"
+            options={elementOptions}
+            className="w-full p-3 border rounded-md"
+          />
+        </div>
+        <div className="w-full md:w-1/2 mt-4 md:mt-0">
+          <label
+            htmlFor="cardCvc"
+            className="block text-sm font-medium text-gray-700"
+          >
+            CVC
+          </label>
+          <CardCvcElement
+            id="cardCvc"
+            options={elementOptions}
+            className="w-full p-3 border rounded-md"
+          />
+        </div>
+      </div>
+      {error && <div className="text-red text-xs">{error}</div>}
+      <button
+        type="submit"
+        disabled={!stripe || loading}
+        className="w-full px-4 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 disabled:bg-indigo-300"
+      >
+        {loading ? "Processing..." : "START 7-DAY TRIAL"}
       </button>
     </form>
   );
