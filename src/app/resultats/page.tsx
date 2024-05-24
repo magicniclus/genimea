578758c816dbcb4d5851f75887cb99c6314cd10a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-24 18:47:21 +0200

    Update page.tsx

--- src/app/resultats/page.tsx
+++ src/app/resultats/page.tsx
@@ -1,21 +1,28 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
-import SubscriptionForm from "@/components/stripe/SubscriptionForm";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
-);
+import Hero from "@/components/Hero";
+import CheckoutForm from "@/components/stripe/CheckoutForm";
 
 const page = () => {
   return (
-    <div>
-      <h1>Resultat</h1>
-      <Elements stripe={stripePromise}>
-        <SubscriptionForm />
-      </Elements>
-    </div>
+    <>
+      <header className="w-full">
+        <div className="max-w-5xl mx-auto rounded-xl flex flex-row items-center justify-start p-6 lg:px-8">
+          <div className="-m-1.5 p-1.5 flex items-center">
+            <span className="sr-only">Genimea</span>
+            <img className="h-8 w-auto" src="/logo.png" alt="logo" />
+            <span className="text-slate-700 text-xl font-bold ml-1">
+              Genimea
+            </span>
+          </div>
+        </div>
+      </header>
+      <main className="relative w-full">
+        <Hero />
+        <CheckoutForm />
+      </main>
+    </>
   );
 };
 
