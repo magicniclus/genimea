7ace005c92c2c7175fc14a7fa4939200289dd163
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-27 16:48:41 +0200

    Update carousel.tsx

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -206,7 +206,7 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute  h-8 w-8 rounded-full",
+        "absolute md:block hidden h-8 w-8 rounded-full",
         orientation === "horizontal"
           ? "-left-9 top-1/2 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -235,7 +235,7 @@ const CarouselNext = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute h-8 w-8 rounded-full",
+        "absolute h-8 w-8 md:block hidden rounded-full",
         orientation === "horizontal"
           ? "-right-9 top-1/2 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
