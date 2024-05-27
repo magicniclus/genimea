8d0c632a40b54ed8f9f30cf177e7f2d4001f1ecb
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-27 17:22:53 +0200

    Update page.tsx

--- src/app/resultats/page.tsx
+++ src/app/resultats/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
@@ -9,8 +10,17 @@ import Hero from "@/components/resultat/Hero";
 import Resultat from "@/components/resultat/Resultat";
 import CheckoutForm from "@/components/stripe/CheckoutForm";
 import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
+import { useEffect, useState } from "react";
 
-const page = () => {
+type User = {
+  firstName: string;
+  lastName: string;
+  age: number;
+  country: string;
+  score: number;
+};
+
+const Page = () => {
   const items = [
     {
       name: "fox",
@@ -34,6 +44,161 @@ const page = () => {
     },
   ];
 
+  const [index, setIndex] = useState(0);
+  const userDynamic = [
+    {
+      firstName: "Carlos",
+      lastName: "Garcia",
+      age: 30,
+      country: "MX",
+      score: 85,
+    },
+    {
+      firstName: "Aiko",
+      lastName: "Tanaka",
+      age: 22,
+      country: "JP",
+      score: 98,
+    },
+    {
+      firstName: "Fatima",
+      lastName: "Benali",
+      age: 28,
+      country: "MA",
+      score: 76,
+    },
+    {
+      firstName: "Liam",
+      lastName: "O'Connor",
+      age: 32,
+      country: "IE",
+      score: 104,
+    },
+    {
+      firstName: "Yara",
+      lastName: "Silva",
+      age: 27,
+      country: "BR",
+      score: 89,
+    },
+    {
+      firstName: "Chen",
+      lastName: "Wang",
+      age: 24,
+      country: "CN",
+      score: 91,
+    },
+    {
+      firstName: "Sophia",
+      lastName: "Schmidt",
+      age: 29,
+      country: "DE",
+      score: 108,
+    },
+    {
+      firstName: "Ivan",
+      lastName: "Petrov",
+      age: 31,
+      country: "RU",
+      score: 92,
+    },
+    {
+      firstName: "Amara",
+      lastName: "Ndiaye",
+      age: 26,
+      country: "SN",
+      score: 95,
+    },
+    {
+      firstName: "Luca",
+      lastName: "Rossi",
+      age: 23,
+      country: "IT",
+      score: 87,
+    },
+    {
+      firstName: "Mia",
+      lastName: "Novak",
+      age: 28,
+      country: "HR",
+      score: 78,
+    },
+    {
+      firstName: "Hassan",
+      lastName: "Ali",
+      age: 27,
+      country: "EG",
+      score: 82,
+    },
+    {
+      firstName: "Anya",
+      lastName: "Kumar",
+      age: 25,
+      country: "IN",
+      score: 90,
+    },
+    {
+      firstName: "Olivia",
+      lastName: "Martin",
+      age: 30,
+      country: "CA",
+      score: 103,
+    },
+    {
+      firstName: "Lucas",
+      lastName: "Mendes",
+      age: 24,
+      country: "PT",
+      score: 88,
+    },
+    {
+      firstName: "Emma",
+      lastName: "Dubois",
+      age: 29,
+      country: "FR",
+      score: 109,
+    },
+    {
+      firstName: "Alex",
+      lastName: "Brown",
+      age: 32,
+      country: "AU",
+      score: 94,
+    },
+    {
+      firstName: "Nina",
+      lastName: "Popova",
+      age: 26,
+      country: "BG",
+      score: 80,
+    },
+    {
+      firstName: "Jorge",
+      lastName: "Fernandez",
+      age: 31,
+      country: "ES",
+      score: 105,
+    },
+    {
+      firstName: "Sara",
+      lastName: "Ahmed",
+      age: 27,
+      country: "SA",
+      score: 97,
+    },
+  ];
+  function generateRandomNumber(min = 4000, max = 10000) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % userDynamic.length);
+    }, generateRandomNumber());
+
+    return () => clearTimeout(timer);
+  }, [index]);
+
   return (
     <>
       <header className="w-full">
@@ -50,11 +215,11 @@ const page = () => {
       <main className="relative w-full">
         <Banner />
         <Hero />
-        <CheckoutForm />
         <div className="w-full flex justify-center">
           <InfiniteMovingCards className="mt-20" items={items} />
         </div>
-        <Resultat />
+        <CheckoutForm />
+        <Resultat index={index} userDynamic={userDynamic} />
         <div className="mt-20">
           <Avis />
         </div>
@@ -67,4 +232,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
