e65eec2d427acfd455838def1b5174e82310c107
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-27 18:36:23 +0200

    Update page.tsx

--- src/app/resultats/page.tsx
+++ src/app/resultats/page.tsx
@@ -10,7 +10,9 @@ import Hero from "@/components/resultat/Hero";
 import Resultat from "@/components/resultat/Resultat";
 import CheckoutForm from "@/components/stripe/CheckoutForm";
 import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
+import { RootState } from "@/redux/store";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 type User = {
   firstName: string;
@@ -20,176 +22,180 @@ type User = {
   score: number;
 };
 
-const Page = () => {
-  const items = [
-    {
-      name: "fox",
-      img: "/icons/fox.svg",
-    },
-    {
-      name: "yahoo",
-      img: "/icons/yahoo.svg",
-    },
-    {
-      name: "Bachart",
-      img: "/icons/Bachart.svg",
-    },
-    {
-      name: "Benzinga",
-      img: "/icons/benzinga.svg",
-    },
-    {
-      name: "MSN",
-      img: "/icons/msn.svg",
-    },
-  ];
+const userDynamic = [
+  {
+    firstName: "Carlos",
+    lastName: "Garcia",
+    age: 30,
+    country: "MX",
+    score: 85,
+  },
+  {
+    firstName: "Aiko",
+    lastName: "Tanaka",
+    age: 22,
+    country: "JP",
+    score: 98,
+  },
+  {
+    firstName: "Fatima",
+    lastName: "Benali",
+    age: 28,
+    country: "MA",
+    score: 76,
+  },
+  {
+    firstName: "Liam",
+    lastName: "O'Connor",
+    age: 32,
+    country: "IE",
+    score: 104,
+  },
+  {
+    firstName: "Yara",
+    lastName: "Silva",
+    age: 27,
+    country: "BR",
+    score: 89,
+  },
+  {
+    firstName: "Chen",
+    lastName: "Wang",
+    age: 24,
+    country: "CN",
+    score: 91,
+  },
+  {
+    firstName: "Sophia",
+    lastName: "Schmidt",
+    age: 29,
+    country: "DE",
+    score: 108,
+  },
+  {
+    firstName: "Ivan",
+    lastName: "Petrov",
+    age: 31,
+    country: "RU",
+    score: 92,
+  },
+  {
+    firstName: "Amara",
+    lastName: "Ndiaye",
+    age: 26,
+    country: "SN",
+    score: 95,
+  },
+  {
+    firstName: "Luca",
+    lastName: "Rossi",
+    age: 23,
+    country: "IT",
+    score: 87,
+  },
+  {
+    firstName: "Mia",
+    lastName: "Novak",
+    age: 28,
+    country: "HR",
+    score: 78,
+  },
+  {
+    firstName: "Hassan",
+    lastName: "Ali",
+    age: 27,
+    country: "EG",
+    score: 82,
+  },
+  {
+    firstName: "Anya",
+    lastName: "Kumar",
+    age: 25,
+    country: "IN",
+    score: 90,
+  },
+  {
+    firstName: "Olivia",
+    lastName: "Martin",
+    age: 30,
+    country: "CA",
+    score: 103,
+  },
+  {
+    firstName: "Lucas",
+    lastName: "Mendes",
+    age: 24,
+    country: "PT",
+    score: 88,
+  },
+  {
+    firstName: "Emma",
+    lastName: "Dubois",
+    age: 29,
+    country: "FR",
+    score: 109,
+  },
+  {
+    firstName: "Alex",
+    lastName: "Brown",
+    age: 32,
+    country: "AU",
+    score: 94,
+  },
+  {
+    firstName: "Nina",
+    lastName: "Popova",
+    age: 26,
+    country: "BG",
+    score: 80,
+  },
+  {
+    firstName: "Jorge",
+    lastName: "Fernandez",
+    age: 31,
+    country: "ES",
+    score: 105,
+  },
+  {
+    firstName: "Sara",
+    lastName: "Ahmed",
+    age: 27,
+    country: "SA",
+    score: 97,
+  },
+];
 
+const items = [
+  {
+    name: "fox",
+    img: "/icons/fox.svg",
+  },
+  {
+    name: "yahoo",
+    img: "/icons/yahoo.svg",
+  },
+  {
+    name: "Bachart",
+    img: "/icons/Bachart.svg",
+  },
+  {
+    name: "Benzinga",
+    img: "/icons/benzinga.svg",
+  },
+  {
+    name: "MSN",
+    img: "/icons/msn.svg",
+  },
+];
+const Page = () => {
   const [index, setIndex] = useState(0);
-  const userDynamic = [
-    {
-      firstName: "Carlos",
-      lastName: "Garcia",
-      age: 30,
-      country: "MX",
-      score: 85,
-    },
-    {
-      firstName: "Aiko",
-      lastName: "Tanaka",
-      age: 22,
-      country: "JP",
-      score: 98,
-    },
-    {
-      firstName: "Fatima",
-      lastName: "Benali",
-      age: 28,
-      country: "MA",
-      score: 76,
-    },
-    {
-      firstName: "Liam",
-      lastName: "O'Connor",
-      age: 32,
-      country: "IE",
-      score: 104,
-    },
-    {
-      firstName: "Yara",
-      lastName: "Silva",
-      age: 27,
-      country: "BR",
-      score: 89,
-    },
-    {
-      firstName: "Chen",
-      lastName: "Wang",
-      age: 24,
-      country: "CN",
-      score: 91,
-    },
-    {
-      firstName: "Sophia",
-      lastName: "Schmidt",
-      age: 29,
-      country: "DE",
-      score: 108,
-    },
-    {
-      firstName: "Ivan",
-      lastName: "Petrov",
-      age: 31,
-      country: "RU",
-      score: 92,
-    },
-    {
-      firstName: "Amara",
-      lastName: "Ndiaye",
-      age: 26,
-      country: "SN",
-      score: 95,
-    },
-    {
-      firstName: "Luca",
-      lastName: "Rossi",
-      age: 23,
-      country: "IT",
-      score: 87,
-    },
-    {
-      firstName: "Mia",
-      lastName: "Novak",
-      age: 28,
-      country: "HR",
-      score: 78,
-    },
-    {
-      firstName: "Hassan",
-      lastName: "Ali",
-      age: 27,
-      country: "EG",
-      score: 82,
-    },
-    {
-      firstName: "Anya",
-      lastName: "Kumar",
-      age: 25,
-      country: "IN",
-      score: 90,
-    },
-    {
-      firstName: "Olivia",
-      lastName: "Martin",
-      age: 30,
-      country: "CA",
-      score: 103,
-    },
-    {
-      firstName: "Lucas",
-      lastName: "Mendes",
-      age: 24,
-      country: "PT",
-      score: 88,
-    },
-    {
-      firstName: "Emma",
-      lastName: "Dubois",
-      age: 29,
-      country: "FR",
-      score: 109,
-    },
-    {
-      firstName: "Alex",
-      lastName: "Brown",
-      age: 32,
-      country: "AU",
-      score: 94,
-    },
-    {
-      firstName: "Nina",
-      lastName: "Popova",
-      age: 26,
-      country: "BG",
-      score: 80,
-    },
-    {
-      firstName: "Jorge",
-      lastName: "Fernandez",
-      age: 31,
-      country: "ES",
-      score: 105,
-    },
-    {
-      firstName: "Sara",
-      lastName: "Ahmed",
-      age: 27,
-      country: "SA",
-      score: 97,
-    },
-  ];
   function generateRandomNumber(min = 4000, max = 10000) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+  const userEmail = useSelector(
+    (state: RootState) => state?.admin?.prospect?.email
+  );
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -202,13 +208,16 @@ const Page = () => {
   return (
     <>
       <header className="w-full">
-        <div className="max-w-6xl mx-auto rounded-xl flex flex-row items-center justify-start py-6 pt-16 px-4 lg:px-8">
+        <div className="max-w-6xl mx-auto rounded-xl flex flex-row items-center justify-between py-6 pt-28 md:pt-16 px-4 lg:px-8">
           <div className="-m-1.5 p-1.5 flex items-center">
             <span className="sr-only">Genimea</span>
             <img className="h-8 w-auto" src="/logo.png" alt="logo" />
             <span className="text-slate-700 text-xl font-bold ml-1">
               Genimea
             </span>
+          </div>
+          <div className="">
+            {userEmail && <p className="text-slate-700">Welcome {userEmail}</p>}
           </div>
         </div>
       </header>
@@ -218,7 +227,7 @@ const Page = () => {
         <div className="w-full flex justify-center">
           <InfiniteMovingCards className="mt-20" items={items} />
         </div>
-        <CheckoutForm />
+        <CheckoutForm index={index} email={userEmail} />
         <Resultat index={index} userDynamic={userDynamic} />
         <div className="mt-20">
           <Avis />
