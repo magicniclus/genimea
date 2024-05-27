6f69c002685896c8c88a1119fc4b0fb5c033364f
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-27 11:21:33 +0200

    Create Banner.tsx

---
+++ src/components/resultat/Banner.tsx
@@ -0,0 +1,23 @@
+const Banner = () => {
+  return (
+    <div className="flex items-center gap-x-6 bg-indigo-600 px-6 py-2.5 sm:px-3.5 justify-center">
+      <a
+        href="#checkout"
+        className="text-sm leading-6 text-white cursor-pointer"
+      >
+        <strong className="font-semibold">Special Welcome Offer</strong>
+        <svg
+          viewBox="0 0 2 2"
+          className="mx-2 inline h-0.5 w-0.5 fill-current"
+          aria-hidden="true"
+        >
+          <circle cx={1} cy={1} r={1} />
+        </svg>
+        Get 85% Discount Today!
+        <span aria-hidden="true">&rarr;</span>
+      </a>
+    </div>
+  );
+};
+
+export default Banner;
