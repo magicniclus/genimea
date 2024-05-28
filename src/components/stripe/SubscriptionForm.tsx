98a76659a66197653610fc558cf4976d04b1510e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-28 09:51:14 +0200

    Update SubscriptionForm.tsx

--- src/components/stripe/SubscriptionForm.tsx
+++ src/components/stripe/SubscriptionForm.tsx
@@ -8,7 +8,11 @@ import {
   useStripe,
 } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import { useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+// Définir les types pour les langues et le contenu
+type Language = "FR" | "EN";
 
 type SubscriptionFormProps = {
   semail?: string;
@@ -19,6 +23,16 @@ const stripePromise = loadStripe(
 );
 
 const SubscriptionForm: React.FC<SubscriptionFormProps> = ({ semail }) => {
+  const searchParams = useSearchParams();
+  const [selectedLang, setSelectedLang] = useState<Language>("FR");
+
+  useEffect(() => {
+    const lang = searchParams?.get("lang");
+    if (lang === "FR" || lang === "EN") {
+      setSelectedLang(lang);
+    }
+  }, [searchParams]);
+
   const stripe = useStripe();
   const elements = useElements();
   const [email, setEmail] = useState(semail || "");
@@ -98,7 +112,75 @@ const SubscriptionForm: React.FC<SubscriptionFormProps> = ({ semail }) => {
     hideIcon: false,
   };
 
-  return (
+  const contentFr = () => (
+    <form onSubmit={handleSubmit} className="space-y-4 w-full">
+      <label
+        htmlFor="cardNumber"
+        className="block text-sm font-medium text-gray-700"
+      >
+        Email utilisateur
+      </label>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        className="w-full px-3 py-3 text-slate-700 border rounded-md focus:outline-none focus:ring-none mb-5"
+      />
+      <div className="space-y-2">
+        <label
+          htmlFor="cardNumber"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Numéro de carte
+        </label>
+        <CardNumberElement
+          id="cardNumber"
+          options={elementOptions}
+          className="w-full p-3 border rounded-md"
+        />
+      </div>
+      <div className="flex md:space-x-4 flex-col md:flex-row">
+        <div className="w-full md:w-1/2">
+          <label
+            htmlFor="cardExpiry"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Date d&apos;expiration (MM/AA)
+          </label>
+          <CardExpiryElement
+            id="cardExpiry"
+            options={elementOptions}
+            className="w-full p-3 border rounded-md"
+          />
+        </div>
+        <div className="w-full md:w-1/2 mt-4 md:mt-0">
+          <label
+            htmlFor="cardCvc"
+            className="block text-sm font-medium text-gray-700"
+          >
+            CVC
+          </label>
+          <CardCvcElement
+            id="cardCvc"
+            options={elementOptions}
+            className="w-full p-3 border rounded-md"
+          />
+        </div>
+      </div>
+      {error && <div className="text-red text-xs">{error}</div>}
+      <button
+        type="submit"
+        disabled={!stripe || loading}
+        className="w-full px-4 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 disabled:bg-indigo-300"
+      >
+        {loading ? "Traitement..." : "COMMENCEZ L'ESSAI DE 7 JOURS"}
+      </button>
+    </form>
+  );
+
+  const contentEn = () => (
     <form onSubmit={handleSubmit} className="space-y-4 w-full">
       <label
         htmlFor="cardNumber"
@@ -112,9 +194,8 @@ const SubscriptionForm: React.FC<SubscriptionFormProps> = ({ semail }) => {
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         required
-        className="w-full px-3 py-3 text-[#a0aec0] border rounded-md focus:outline-none focus:ring-none mb-5"
+        className="w-full px-3 py-3 text-slate-700 border rounded-md focus:outline-none focus:ring-none mb-5"
       />
-      {/* <div className="w-[70%] h-0.5 bg-slate-200 mx-auto" /> */}
       <div className="space-y-2">
         <label
           htmlFor="cardNumber"
@@ -166,6 +247,8 @@ const SubscriptionForm: React.FC<SubscriptionFormProps> = ({ semail }) => {
       </button>
     </form>
   );
+
+  return <>{selectedLang === "FR" ? contentFr() : contentEn()}</>;
 };
 
 export default SubscriptionForm;
