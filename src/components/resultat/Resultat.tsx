c1e4c9c88890c7fb6f6dc5c1e73d208c077377ad
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-28 09:51:10 +0200

    Update Resultat.tsx

--- src/components/resultat/Resultat.tsx
+++ src/components/resultat/Resultat.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 /* eslint-disable react-hooks/exhaustive-deps */
+import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import Flag from "react-world-flags";
 
@@ -15,8 +18,19 @@ type ResultatProps = {
   userDynamic: User[];
 };
 
+type Language = "FR" | "EN";
+
 const Resultat = ({ index, userDynamic }: ResultatProps) => {
   const [users, setUsers] = useState<User[]>([userDynamic[0]]);
+  const searchParams = useSearchParams();
+  const [selectedLang, setSelectedLang] = useState<Language>("FR"); // Par défaut 'FR'
+
+  useEffect(() => {
+    const lang = searchParams?.get("lang"); // Ajouter une vérification nulle pour searchParams
+    if (lang === "FR" || lang === "EN") {
+      setSelectedLang(lang);
+    }
+  }, [searchParams]);
 
   useEffect(() => {
     const newUser = userDynamic[index];
@@ -28,9 +42,47 @@ const Resultat = ({ index, userDynamic }: ResultatProps) => {
       if (newUsers.length > 6) {
         newUsers.pop();
       }
-      return newUsers; // Add this line to return the new state value
+      return newUsers;
     });
   }, [index]);
+
+  const contentFr = () => (
+    <div className="flex mx-auto items-center transition flex-col md:flex-row">
+      <p className="transition">
+        Nouvel utilisateur : {"   "}
+        <strong>
+          {userDynamic[index]?.firstName} {userDynamic[index]?.lastName}
+        </strong>{" "}
+        de{" "}
+      </p>
+      <Flag
+        code={userDynamic[index]?.country}
+        className="h-5 w-auto rounded-md mx-2 transition"
+      />
+      <p className="transition">
+        a un score de <strong>{userDynamic[index]?.score}</strong>!
+      </p>
+    </div>
+  );
+
+  const contentEn = () => (
+    <div className="flex mx-auto items-center transition flex-col md:flex-row">
+      <p className="transition">
+        New user: {"   "}
+        <strong>
+          {userDynamic[index]?.firstName} {userDynamic[index]?.lastName}
+        </strong>{" "}
+        from{" "}
+      </p>
+      <Flag
+        code={userDynamic[index]?.country}
+        className="h-5 w-auto rounded-md mx-2 transition"
+      />
+      <p className="transition">
+        has a score of <strong>{userDynamic[index]?.score}</strong>!
+      </p>
+    </div>
+  );
 
   return (
     <>
@@ -46,27 +98,12 @@ const Resultat = ({ index, userDynamic }: ResultatProps) => {
       <section style={{ zIndex: 200 }}>
         <div className="w-screen bg-slate-100 text-slate-700 fixed top-0 right-0 py-3 z-50">
           <div className="max-w-6xl mx-auto flex items-center justify-between px-4 w-full">
-            <div className="flex mx-auto items-center transition flex-col md:flex-row">
-              <p className="transition">
-                New user: {"   "}
-                <strong>
-                  {userDynamic[index]?.firstName} {userDynamic[index]?.lastName}
-                </strong>{" "}
-                from{" "}
-              </p>
-              <Flag
-                code={userDynamic[index]?.country}
-                className="h-5 w-auto rounded-md mx-2 transition"
-              />
-              <p className="transition">
-                has a score of <strong>{userDynamic[index]?.score}</strong>!
-              </p>
-            </div>
+            {selectedLang === "FR" ? contentFr() : contentEn()}
           </div>
         </div>
         <div className="max-w-6xl mx-auto rounded-xl flex flex-col items-center justify-start py-6 pt-12 lg:px-8">
           <h2 className="text-3xl text-slate-700 font-bold mt-3 mx-auto mb-7">
-            Latest results
+            {selectedLang === "FR" ? "Derniers résultats" : "Latest results"}
           </h2>
           <div className="flex flex-col md:flex-row w-full justify-between">
             <div className="w-full col-span-1 sm:col-span-2 grid grid-cols-1 sm:grid-cols-2 gap-4">
@@ -88,7 +125,8 @@ const Resultat = ({ index, userDynamic }: ResultatProps) => {
                     </h3>
                     <p className="text-sm ml-2"> {user?.lastName}</p>
                   </div>
-                  IQ = {user?.score}
+                  {selectedLang === "FR" ? "QI = " : "IQ = "}
+                  {user?.score}
                 </div>
               ))}
             </div>
