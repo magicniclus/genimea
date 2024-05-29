6c5b066b31021b1e1dcce57c1db4c04029a3e7b4
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-29 15:54:20 +0200

    Create rootReducer.ts

---
+++ src/redux/rootReducer.ts
@@ -0,0 +1,13 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import adminReducer from "./addProspect";
+import userReducer from "./dataUserManager";
+
+const rootReducer = combineReducers({
+  admin: adminReducer,
+  user: userReducer,
+  // Ajoutez d'autres slices ici si nécessaire
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export default rootReducer;
