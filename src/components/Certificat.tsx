21f4adc46e6551020f73b4ca5b7b3c9f8f07c85e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-31 17:38:48 +0200

    Update Certificat.tsx

--- src/components/Certificat.tsx
+++ src/components/Certificat.tsx
@@ -28,7 +28,7 @@ const CertificatContent = () => {
           </h2>
           <div className="mt-10 flex items-center gap-x-6">
             <a
-              href="#"
+              href={"/test" + "?lang=FR"}
               className="px-6 py-2 bg-textBlue rounded-md text-white text-2xl hover:text-textBlue hover:bg-white hover:border hover:border-textBlue hover:border-2 transition duration-300 ease-in-out text-center md:text-start group flex max-w-[fit-content]"
             >
               Commencer mon test
@@ -51,22 +51,20 @@ const CertificatContent = () => {
   const contentEn = () => {
     return (
       <div className="mx-auto max-w-6xl px-4 py-24 sm:py-32 flex justify-between md:flex-row flex-col w-full items-center">
-        <div className="w-6/12">
+        <div className="md:w-6/12 w-full flex flex-col">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Get your
-            <br />
-            <span className="text-textBlue">IQ certificate</span>
+            Get your <span className="text-textBlue">IQ certificate</span>
           </h2>
           <div className="mt-10 flex items-center gap-x-6">
             <a
-              href="#"
+              href={"/test" + "?lang=EN"}
               className="px-6 py-2 bg-textBlue rounded-md text-white text-2xl hover:text-textBlue hover:bg-white hover:border hover:border-textBlue hover:border-2 transition duration-300 ease-in-out text-center md:text-start group flex max-w-[fit-content]"
             >
               Start my test
             </a>
           </div>
         </div>
-        <div className="w-6/12 flex justify-center">
+        <div className="md:w-6/12 w-full flex justify-center md:mt-0 mt-10">
           <div className="overflow-hidden rounded-md w-full shadow-lg">
             <img
               src="./images/certificatEn.png"
