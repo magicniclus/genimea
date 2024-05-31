79ea1b8e2350123ee1654a78a3465f65dece157a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-31 09:40:54 +0200

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,10 +4,8 @@
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import LanguageSelect from "./buttons/LanguageSelect";
+import { Suspense, useEffect, useState } from "react";
 
-// Define the types for the languages and content
 type Language = "FR" | "EN";
 interface Content {
   espace: string;
@@ -33,18 +31,8 @@ const content: Record<Language, Content> = {
   },
 };
 
-const Nav = () => {
+const Nav = ({ selectedLang }: { selectedLang: Language }) => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const searchParams = useSearchParams();
-  const [selectedLang, setSelectedLang] = useState<Language>("FR"); // Default to 'FR'
-
-  // Update the language state based on URL search parameters
-  useEffect(() => {
-    const lang = searchParams?.get("lang");
-    if (lang === "FR" || lang === "EN") {
-      setSelectedLang(lang);
-    }
-  }, [searchParams]);
 
   const handleScroll = (
     e: React.MouseEvent<HTMLAnchorElement>,
@@ -58,109 +46,137 @@ const Nav = () => {
   };
 
   return (
-    <header className="bg-textBlue">
-      <nav
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-        aria-label="Global"
-      >
-        <a
-          href={"/" + (selectedLang === "FR" ? "?lang=FR" : "")}
-          className="-m-1.5 p-1.5 flex items-center"
+    <>
+      <header className="bg-textBlue">
+        <nav
+          className="mx-auto flex max-w-5xl items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
         >
-          <span className="sr-only">Genimea</span>
-          <img className="h-8 w-auto" src="./logoWhite.png" alt="logo" />
-          <span className="text-white text-xl font-bold ml-1">Genimea</span>
-        </a>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6 text-white" aria-hidden="true" />
-          </button>
-        </div>
-        <div className="hidden lg:flex lg:gap-x-12">
-          {content[selectedLang].navigation.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              onClick={(e) => handleScroll(e, item.href)}
-              className="text-sm font-semibold leading-6 text-white"
-            >
-              {item.name}
-            </a>
-          ))}
-          <LanguageSelect />
           <a
-            href={"/signin" + (selectedLang === "FR" ? "?lang=FR" : "?lang=EN")}
-            className="text-sm font-semibold leading-6 text-white px-2 border border-white rounded-md"
+            href={"/" + (selectedLang === "FR" ? "?lang=FR" : "?lang=EN")}
+            className="-m-1.5 p-1.5 flex items-center"
           >
-            {content[selectedLang].espace}
+            <span className="sr-only">Genimea</span>
+            <img className="h-8 w-auto" src="./logoWhite.png" alt="logo" />
+            <span className="text-white text-xl font-bold ml-1">Genimea</span>
           </a>
-        </div>
-      </nav>
-      <Dialog
-        className="lg:hidden"
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-      >
-        <div className="fixed inset-0 z-10 " />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 bg-textBlue">
-          <div className="flex items-center justify-between">
-            <a
-              href={"/" + (selectedLang === "FR" ? "?lang=FR" : "")}
-              className="-m-1.5 p-1.5 flex items-center"
-            >
-              <span className="sr-only">Genimea</span>
-              <img className="h-8 w-auto" src="./logoWhite.png" alt="logo" />
-              <span className="text-white text-xl font-bold ml-2">Genimea</span>
-            </a>
+          <div className="flex lg:hidden">
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(true)}
             >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6 text-white" aria-hidden="true" />
             </button>
           </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-white">
-              <div className="space-y-2 py-6">
-                {content[selectedLang].navigation.map((item) => (
+          <div className="hidden lg:flex lg:gap-x-12">
+            {content[selectedLang].navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                onClick={(e) => handleScroll(e, item.href)}
+                className="text-sm font-semibold leading-6 text-white"
+              >
+                {item.name}
+              </a>
+            ))}
+            <a
+              href={
+                "/signin" + (selectedLang === "FR" ? "?lang=FR" : "?lang=EN")
+              }
+              className="text-sm font-semibold leading-6 text-white px-2 border border-white rounded-md"
+            >
+              {content[selectedLang].espace}
+            </a>
+          </div>
+        </nav>
+        <Dialog
+          className="lg:hidden"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+        >
+          <div className="fixed inset-0 z-10 " />
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 bg-textBlue">
+            <div className="flex items-center justify-between">
+              <a
+                href={"/" + (selectedLang === "FR" ? "?lang=FR" : "?lang=EN")}
+                className="-m-1.5 p-1.5 flex items-center"
+              >
+                <span className="sr-only">Genimea</span>
+                <img className="h-8 w-auto" src="./logoWhite.png" alt="logo" />
+                <span className="text-white text-xl font-bold ml-2">
+                  Genimea
+                </span>
+              </a>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-white">
+                <div className="space-y-2 py-6">
+                  {content[selectedLang].navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      onClick={(e) => {
+                        handleScroll(e, item.href);
+                        setMobileMenuOpen(false);
+                      }}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-textBlue/70"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="py-6">
                   <a
-                    key={item.name}
-                    href={item.href}
-                    onClick={(e) => {
-                      handleScroll(e, item.href);
-                      setMobileMenuOpen(false);
-                    }}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-textBlue/70"
+                    href={
+                      "/signin" +
+                      (selectedLang === "FR" ? "?lang=FR" : "?lang=EN")
+                    }
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-textBlue/70"
                   >
-                    {item.name}
+                    {content[selectedLang].espace}
                   </a>
-                ))}
-              </div>
-              <div className="py-6">
-                <a
-                  href={
-                    "/signin" +
-                    (selectedLang === "FR" ? "?lang=FR" : "?lang=EN")
-                  }
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-textBlue/70"
-                >
-                  {content[selectedLang].espace}
-                </a>
-                <LanguageSelect />
+                </div>
               </div>
             </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
-    </header>
+          </Dialog.Panel>
+        </Dialog>
+      </header>
+    </>
   );
 };
 
-export default Nav;
+const Page = () => {
+  const searchParams = useSearchParams();
+  const [selectedLang, setSelectedLang] = useState<Language>("FR");
+
+  useEffect(() => {
+    const lang = searchParams?.get("lang");
+    if (lang === "FR" || lang === "EN") {
+      setSelectedLang(lang);
+    }
+  }, [searchParams]);
+
+  return (
+    <Suspense
+      fallback={
+        <div className="h-screen w-screen bg-slate-50 flex justify-center items-center animate-pulse duration-2000 ease-in-out">
+          <img src="/logo.png" alt="logo" className="w-20 h-auto" />
+        </div>
+      }
+    >
+      <Nav selectedLang={selectedLang} />
+    </Suspense>
+  );
+};
+
+export default Page;
